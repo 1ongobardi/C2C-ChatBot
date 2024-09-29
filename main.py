e6cb10b8b2577abae654d3833ade876e3a20d9c6
Angel <angelrlongobardi@gmail.com>
2024-09-29 21:17:33 +0000

    print("Welcome to the Elite 101 ChatBot!") user_name = input("Please enter your name: ") age = input("Nice to meet you" + user_name + "! How old are you? ") print("Welcome" + user_name + "! Oh, to be " + age + " again! How can I help you today?")
    def user_choice():
        print("Please choose one from the following options: ")
        print(" 1. Placeholder Option 1")
        print(" 2. Placeholder Option 2")
        print(" 3. Placeholder Option 3")
        print(" 4. Exit the conversation")
    
        if user_choice == 1:
            print("")
        elif 2:
            print("")
        elif 3:
            print("")
        elif 4:
            print("Goodbye, " + user_name + "Zach! Have a great day!")
        else:
            print("Sorry, that was not a valid response. Try again!")

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Welcome to the Elite 101 ChatBot!")
+user_name = input("Please enter your name: ")
+age = input("Nice to meet you" + user_name + "! How old are you? ")
+print("Welcome" + user_name + "! Oh, to be " + age + " again! How can I help you today?")
+
+def user_choice():
+    print("Please choose one from the following options: ")
+    print(" 1. Placeholder Option 1")
+    print(" 2. Placeholder Option 2")
+    print(" 3. Placeholder Option 3")
+    print(" 4. Exit the conversation")
+
+    if user_choice == 1:
+        print("")
+    elif 2:
+        print("")
+    elif 3:
+        print("")
+    elif 4:
+        print("Goodbye, " + user_name + "Zach! Have a great day!")
+    else:
+        print("Sorry, that was not a valid response. Try again!")
